29fb25737bf9c89a2e398c2a6a0879f188c5ba64
Tanvir Ahmed <parthotanvir1234@gmail.com>
2022-09-30 16:35:19 +0600

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <nav className="header">
+        <h2>FITNESS TOOLS</h2>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
